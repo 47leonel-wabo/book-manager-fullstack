3be900c01d006c3b2331dbfae0d328fafaa5fdf7
leonel47 <waboleonel@gmail.com>
2021-08-28 22:29:37 -0400

    Backend, initial commit

---
+++ books-backend/src/test/java/com/assa/bookdatabase/books/BooksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assa.bookdatabase.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
