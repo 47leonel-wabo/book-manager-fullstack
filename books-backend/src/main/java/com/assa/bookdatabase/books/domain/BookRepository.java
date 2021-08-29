ccddf35116b1b236f463a96c26a446dd9a3ce2f8
leonel47 <waboleonel@gmail.com>
2021-08-29 07:04:40 -0400

    End creating RESTful web service

--- books-backend/src/main/java/com/assa/bookdatabase/books/domain/BookRepository.java
+++ books-backend/src/main/java/com/assa/bookdatabase/books/domain/BookRepository.java
@@ -1,19 +1,22 @@
 package com.assa.bookdatabase.books.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findByTopic(final String topic);
+    List<Book> findByTopic(final @Param("topic") String topic);
 
-    Optional<Book> findByIsbn(final String isbn);
+    Optional<Book> findByIsbn(final @Param("isbn") String isbn);
 
-    List<Book> findByPrice(final BigDecimal price);
+    List<Book> findByPrice(final @Param("price") BigDecimal price);
 
-    List<Book> findByAuthor(final Author author);
+    List<Book> findByAuthor(final @Param("author") Author author);
 
 }
