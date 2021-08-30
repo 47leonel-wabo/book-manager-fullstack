c442c03b1e2ad42190b510ad05239e53e1963ff7
leonel47 <waboleonel@gmail.com>
2021-08-30 10:45:08 -0400

    Implementing delete functionality

--- book-frontend/src/components/BookList.jsx
+++ book-frontend/src/components/BookList.jsx
@@ -1,55 +1,110 @@
 import React from "react";
+import { useMemo, useState } from "react";
+import { useTable } from "react-table";
 import * as CONSTANT from "../constant";
 
-class BookList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [],
-            isLoading: false,
-        };
-    }
+const BookList = (props) => {
+    const [books, setBooks] = useState([]);
 
-    componentDidMount() {
-        this.setState({
-            isLoading: true,
-        });
+    const fetchBookData = () => {
         fetch(CONSTANT.SERVER_URL.concat("/api/books"))
             .then((response) => response.json())
             .then((data) => {
-                this.setState({
-                    books: data._embedded.books,
-                    isLoading: false,
-                });
+                setBooks(data._embedded.books);
             })
             .catch((error) => console.error(error));
-    }
+    };
 
-    render() {
-        const { isLoading, books } = this.state;
+    useMemo(() => {
+        fetchBookData();
+    }, []);
 
-        if (isLoading) {
-            return <h4>Loading data...</h4>;
-        } else {
-            const tableRows = books.map((book, index) => (
-                <tr key={index}>
-                    <td>{book.title}</td>
-                    <td>{book.publisher}</td>
-                    <td>{book.topic}</td>
-                    <td>{book.isbn}</td>
-                    <td>{book.publicationDate}</td>
-                    <td>{book.price}</td>
-                </tr>
-            ));
-            return (
-                <>
-                    <table>
-                        <tbody>{tableRows}</tbody>
-                    </table>
-                </>
-            );
-        }
-    }
-}
+    const handleDeletion = (link) => {
+        fetch(link, { method: "DELETE" })
+            .then((response) => fetchBookData())
+            .catch((error) => console.error(error));
+    };
+
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: "Title",
+                accessor: "title",
+            },
+            {
+                Header: "Publisher",
+                accessor: "publisher",
+            },
+            {
+                Header: "Topic",
+                accessor: "topic",
+            },
+            {
+                Header: "ISBN",
+                accessor: "isbn",
+            },
+            {
+                Header: "Publication Date",
+                accessor: "publicationDate",
+            },
+            {
+                Header: "Price $",
+                accessor: "price",
+            },
+            {
+                id: "delbutton",
+                sortable: false,
+                filterable: false,
+                accessor: "_links.self.href",
+                Cell: ({ value }) => (
+                    <button onClick={() => handleDeletion(value)}>
+                        Delete
+                    </button>
+                ),
+            },
+        ],
+        []
+    );
+
+    const tableInstance = useTable({ columns, data: books });
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        tableInstance;
+
+    return (
+        <>
+            <table {...getTableProps}>
+                <thead>
+                    {headerGroups.map((hg) => (
+                        <tr {...hg.getHeaderGroupProps()}>
+                            {hg.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render("Header")}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render("Cell")}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
+    // }
+};
 
 export default BookList;
