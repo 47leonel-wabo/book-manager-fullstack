a0489d298131e5b7d31616b516cc17896223a689
leonel47 <waboleonel@gmail.com>
2021-08-31 09:55:57 -0400

    Adding enzyme and unofficial  enzyme adapter for react

--- book-frontend/src/App.test.js
+++ book-frontend/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import TestRenderer from "react-test-renderer";
+import AddBook from "./components/AddBook";
 
 test("renders book manager", () => {
     render(<App />);
@@ -11,4 +13,9 @@ test("renders export in csv link", () => {
     render(<App />);
     const exportCsvLink = screen.getByText(/Export CSV/i);
     expect(exportCsvLink).toBeInTheDocument();
+});
+
+it("render a snapshot", () => {
+    const tree = TestRenderer.create(<AddBook />).toJSON();
+    expect(tree).toMatchSnapshot();
 });
